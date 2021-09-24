f5df797caee79f38ce9f549307e5f957858bf7c7
User <it20249366@my.sliit.lk>
2021-09-24 15:03:36 +0530

    add strings

--- app/src/main/java/com/example/nox/MainActivity4.java
+++ app/src/main/java/com/example/nox/MainActivity4.java
@@ -1,14 +1,28 @@
 package com.example.nox;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity4 extends AppCompatActivity {
-
+ private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main4);
+
+
+        toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
     }
 }
