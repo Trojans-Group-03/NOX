4a327c0b0400e215b358168f40aad8be1bac607f
User <it20249366@my.sliit.lk>
2021-09-25 17:15:41 +0530

    connect to firebase

--- app/src/main/java/com/example/nox/MainActivity2.java
+++ app/src/main/java/com/example/nox/MainActivity2.java
@@ -3,12 +3,38 @@ package com.example.nox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    TextInputLayout regName,regEmail,regPassword;
+    Button regbtn,reglogin;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        regName =findViewById(R.id.username);
+        regEmail =findViewById(R.id.email);
+        regPassword = findViewById(R.id.password);
+        regbtn = findViewById(R.id.signUp);
+        reglogin = findViewById(R.id.reglogin);
+
+        regbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rootNode = FirebaseDatabase.getInstance();
+
+            }
+        });
     }
 }
